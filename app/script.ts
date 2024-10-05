e0e3d250fab90396fec993bb5c6801fc16688308
João Zenaro <joaoz@email.com>
2024-10-05 18:10:32 +0000

    feat: general cleanup and dev container improvements

--- app/script.ts
+++ app/script.ts
@@ -2,19 +2,25 @@ import { Types } from "ably/promises";
 import * as Ably from "ably/promises";
 
 (async () => {
-
-    console.log("Oh hai! 🖤");
-
     const optionalClientId = "optionalClientId"; // When not provided in authUrl, a default will be used.
     const ably = new Ably.Realtime.Promise({ authUrl: `/api/ably-token-request?clientId=${optionalClientId}` });
     const channel = ably.channels.get("some-channel-name");
 
     await channel.subscribe((msg: Types.Message) => {
         console.log("Ably message received", msg);
-        document.getElementById("response").innerHTML += "<br />" + JSON.stringify(msg);
+
+        document.querySelector("#messages").innerHTML += `${msg.clientId}: ${msg.data?.message ?? ""}`;
     });
 
-    channel.publish("hello-world-message", { message: "Hello world!" });
+    document.querySelector("#send-message").addEventListener("submit", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const data = new FormData(e.target as HTMLFormElement);
+        const formValues = Object.fromEntries(data.entries());
+
+        channel.publish("channel-message", { message: formValues?.message ?? "" });
+    })
 })();
 
 export { };
